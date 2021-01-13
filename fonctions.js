7d922b74074e570a9b4bfa27f6dcccf54b4f80f1
Christopher Billy <billy_christopher@hotmail.com>
2021-01-13 10:26:17 +0100

    Ajout fonction obtenir tous les titres selon une date donnée

--- fonctions.js
+++ fonctions.js
@@ -1,5 +1,5 @@
 
-module.exports = { readJson, writeJson, getAllTitleAndYear, convertToYear };
+module.exports = { readJson, writeJson, getAllTitleAndYear, getAllMoviesByDate };
 
 const fs = require('fs');
 
@@ -10,16 +10,15 @@ function readJson(path) {
     return movies;
 }
 
-
 function writeJson(data, outputfile) {
-    let donnees = JSON.stringify(data);
+    let donnees = JSON.stringify(data, null, '\t');
     fs.writeFileSync(outputfile, donnees);
 }
 
 function getAllTitleAndYear(array) {
     let result = array.map (elem => {
         let year = convertToYear(elem.release_date);
-        elem.title = elem.title + " (" + year + ")";
+        elem.title += " (" + year + ")";
         return elem;
     });
     return result;
@@ -28,4 +27,18 @@ function getAllTitleAndYear(array) {
 function convertToYear(times) {
     const year = new Date(times * 1000).getFullYear();
     return year;
+}
+
+function getAllMoviesByDate(array, years) {
+    let found = false;
+    array.forEach (elem => {
+        let year = convertToYear(elem.release_date);
+        if (year === years) {
+            found = true;
+            console.log("Titre : " + elem.title + " (" + year + ")");
+        }
+    });
+    if (!found) {
+        console.log("Aucun Film correspondant à l\'année " + years + " n'a été trouvé")
+    }
 }
--- main.js
+++ main.js
@@ -2,14 +2,15 @@
 const f = require('./fonctions.js');
 
 
-let args = process.argv;
+let args = process.argv; // On récupère les arguments en CLI
+let start = new Date().getTime(); // Début du temps d'exécution du programme
 
-const movies = f.readJson('./test.json');
-const data = f.getAllTitleAndYear(movies);
-f.writeJson(data, './modif.json');
-const movies2 = f.readJson('./modif.json');
+const movies = f.readJson('./movies.json');
+// const data = f.getAllTitleAndYear(movies);
+// f.writeJson(data, './modif.json');
+// const movies2 = f.readJson('./modif.json');
 
-console.log(movies[0].title);
-console.log(movies2[0]);
+f.getAllMoviesByDate(movies, 2018);
 
-// console.log();
+let stop = new Date().getTime(); // Début du temps d'exécution du programme
+console.log("Execution time : " + (stop-start) + " ms"); // Afficher temps d'exécution du programme
