83f46b4d6e759d7a2d7baf751c045464d50971c1
Christopher Billy <billy_christopher@hotmail.com>
2021-01-14 12:11:52 +0100

    fix conflits

--- main.js
+++ main.js
@@ -9,17 +9,16 @@ let args = process.argv; // On récupère les arguments en CLI
 
 if (args[2] === undefined) {
     console.log("Veuillez entrer \"-save\" ou \"-action\"");
-} 
+}
 else {
     if (args.includes("-action")) {
         const index = f.getIndex(args, "-action");
 
         if (args[index + 1] === "transform" || args[index + 1] === "sort_date" ||
-        args[index + 1] === "sort_title" || args[index + 1] === "search_date" ||
-        args[index + 1] === "search_keyword") {
+            args[index + 1] === "sort_title" || args[index + 1] === "search_date" ||
+            args[index + 1] === "search_keyword") {
 
-<<<<<<< HEAD
-            if (args[index + 2]) {
+            if (f.checkParamFile(args[index + 2], args[index + 3])) {
                 const movies = f.readJson(args[index + 2]);
                 let data;
                 // J'observe l'argument à l'index["-action" + 1]
@@ -62,79 +61,37 @@ else {
                                 f.display(movies, min, max, false);
                             }
                         }
-                        
+                        if (args[index + 4] !== "true" && args[index + 4] !== "false") {
+                            console.log("Veuillez préciser si la liste passé en entré est trié. (true / false)");
+                        }
                         break;
                     case "search_keyword":  // 5: keyword  6: genre
-                        console.log("keyword: " + args[index + 3] + " | genre: " + args[index + 4]);
-                        data = f.tri_rapide(movies, 0, movies.length - 1, "release_date");
-                        const movie = f.getAllMoviesByGenre(f.getAllMoviesByKey(data, args[index + 3]), args[index + 4]);
-                        if (args.includes("-save")) {
-                            const indexSave = f.getIndex(args, "-save");
-                            f.downloadImg(movie.poster, args[indexSave + 1], movie.title.replace(/[^a-zA-Z0-9]/g));
-                            console.log("Film trouvé : " + movie.title);
+                        if (!args[index + 3]) {
+                            console.log("Veuillez entrer un mot clé !");
+                        } else if (!args[index + 4]) {
+                            console.log("Veuillez entrer un genre !");
                         }
                         else {
-                            console.log("Film trouvé : " + movie.title);
+                            console.log("keyword: " + args[index + 3] + " | genre: " + args[index + 4]);
+                            data = f.tri_rapide(movies, 0, movies.length - 1, "release_date");
+                            const movie = f.getAllMoviesByGenre(f.getAllMoviesByKey(data, args[index + 3]), args[index + 4]);
+                            if (args.includes("-save")) {
+                                const indexSave = f.getIndex(args, "-save");
+                                f.downloadImg(movie.poster, args[indexSave + 1], movie.title.replace(/[^a-zA-Z0-9]/g));
+                                console.log("Film trouvé : " + movie.title);
+                            }
+                            else {
+                                console.log("Film trouvé : " + movie.title);
+                            }
                         }
                         break;
 
                     default:
                         break;
-=======
-            // if (args[index + 2]) {
-                if(f.checkParamFile(args[index + 2], args[index + 3])){
-                    const movies = f.readJson(args[index + 2]);
-                    let data;
-                    // J'observe l'argument à l'index["-action" + 1]
-                    switch (args[index + 1]) {
-                        case "transform":
-                            data = f.getAllTitleAndYear(movies);
-                            f.writeJson(data, args[index + 3]);
-                            break;
-                        case "sort_date":
-                            data = f.tri_rapide(movies, 0, movies.length - 1, "release_date");
-                            data = f.getAllTitleAndYear(movies);
-                            f.writeJson(data, args[index + 3]);
-                            break;
-                        case "sort_title":
-                            data = f.tri_rapide(movies, 0, movies.length - 1, "title");
-                            f.writeJson(data, args[index + 3]);
-                            break;
-                        case "search_date":
-                            if (args[index + 4] === "false") {
-                                f.getAllMoviesByDate(movies, args[index + 3]);
-                            }
-                            if (args[index + 4] === "true") {
-                                const indexFound = f.searchBinarie(movies, 0, movies.length - 1, args[index + 3]);
-                                const min = f.decremente(movies, indexFound, args[index + 3]);
-                                const max = f.incremente(movies, indexFound, args[index + 3]);
-                                f.display(movies, min, max);
-                            }
-                            if(args[index + 4] !== "true" && args[index + 4] !== "false"){
-                                console.log("Veuillez préciser si la liste passé en entré est trié. (true / false)");
-                            }
-                            break;
-                        case "search_keyword":  // 5: keyword  6: genre
-                            if(!args[index + 3]){
-                                console.log("Veuillez entrer un mot clé !");
-                            } else if(!args[index + 4]){
-                                console.log("Veuillez entrer un genre !");
-                            } else {
-                                console.log("keyword: " + args[index + 3] + " | genre: " + args[index + 4] + "\n");
-                                data = f.tri_rapide(movies, 0, movies.length - 1, "release_date");
-                                console.log(f.getAllMoviesByGenre(f.getAllMoviesByKey(data, args[index + 3]), args[index + 4]));
-                            }
-                            break;
-                            
-
-                        default:
-                            break;
-                    }
->>>>>>> Nohan
                 }
-            // }
+            }
         }
-        else if(args[3] === undefined){
+        else if (args[index + 1] === undefined) {
             console.log("Action List : \n - transform <input.json> <output.json> \n - sort_date <input.json> <output.json> \n - sort_title <input.json> <output.json> \n - search_date <input.json> <year> <sorted?> \n - search_keyword <input.json> <keyword> <genre>");
         } else {
             console.log("<" + args[3] + ">" + " n'est pas reconnu comme une action. Veuillez entrer une action valide !");
@@ -143,4 +100,4 @@ else {
 }
 
 let stop = new Date().getTime(); // Fin du temps d'exécution du programme
-console.log("Execution time : " + (stop-start) + " ms"); // Afficher temps d'exécution du programme
+console.log("Execution time : " + (stop - start) + " ms"); // Afficher temps d'exécution du programme
